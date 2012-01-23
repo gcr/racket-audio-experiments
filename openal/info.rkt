e02d6cfbf742894b4ac4e5d3e8acceb3fad6e24a
Michael W <gcr@sneakygcr.net>
2012-01-22 19:01:46 -0700

    Packaged openal in PLaneT

---
+++ openal/info.rkt
@@ -0,0 +1,7 @@
+#lang setup/infotab
+(define name "OpenAL Bindings")
+(define blurb
+  '("Some simple OpenAL bindings for Racket."))
+(define primary-file "main.rkt")
+(define categories '(media))
+(define repositories '("4.x"))
